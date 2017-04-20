9c2e5fcd8f22270d1bd9387e7506a621ffea7217
Andrew Kroshko <akroshko.public+devel@gmail.com>
2017-04-20 17:29:03 -0600

    Comment change.

--- zathura-local.sh
+++ zathura-local.sh
@@ -2,7 +2,6 @@
 main () {
     DOCUMENTMASTER="$1"
     zathura "$@"
-    # TODO: obviously fix this before commit
     # TODO: want something that does not raise
     sleep 0.2
     WINNAME=$(wmctrl -lx | awk '{print $1 " " $3 " " $5}' | grep zathura.Zathura | grep "$DOCUMENTMASTER" | awk '{print $3}')
