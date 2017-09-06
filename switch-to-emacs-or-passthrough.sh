eb7d67e595c20f91697cf7e5788b3f82e3fa6d7f
Andrew Kroshko <akroshko.public+devel@gmail.com>
2017-09-06 13:58:46 -0600

    A few new things.  Switch-to-emacs specific to my machine.

--- switch-to-emacs-or-passthrough.sh
+++ switch-to-emacs-or-passthrough.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-echo "test"
+# TODO: what does this do? seems to check if emacs is current window
 if wmctrl -lx | awk '{print $1 " " $3}' | sed -e 's/0x0*//g' | grep "$(xprop -root | grep ^_NET_ACTIVE_WINDOW | awk '{print $5}' | sed 's/,//' | sed -e 's/0x0*//g')" | grep emacs.Emacs; then
-    # open xpdf or switch to other remote
-    # TODO: non-released key combo
-    xdotool key alt+"A"
+    # let alt+"e" pass through
+    # XXXX: change this to something else if using key other than alt+"e"
+    # XXXX: emacs command alt+"e" is next frame (could be something to switch out of emacs)
+    # xdotool key meta+e
+    /home/akroshko/cic-vcs/bash-stdlib/launch-emacsclient noframe --eval "(other-frame 1)"
 else
     wmctrl -x -a emacs.Emacs
 fi
