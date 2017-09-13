696af5db66fbc6bcd5b31ad0353320bf778df571
Andrew Kroshko <akroshko.public+devel@gmail.com>
2017-09-13 00:42:36 -0600

    Slight changes.

--- switch-to-emacs-or-passthrough.sh
+++ switch-to-emacs-or-passthrough.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-# TODO: what does this do? seems to check if emacs is current window
+# TODO: what does this do? seems to check if emacs is current active window, put into function and test?
 if wmctrl -lx | awk '{print $1 " " $3}' | sed -e 's/0x0*//g' | grep "$(xprop -root | grep ^_NET_ACTIVE_WINDOW | awk '{print $5}' | sed 's/,//' | sed -e 's/0x0*//g')" | grep emacs.Emacs; then
     # let alt+"e" pass through
     # XXXX: change this to something else if using key other than alt+"e"
     # XXXX: emacs command alt+"e" is next frame (could be something to switch out of emacs)
-    # xdotool key meta+e
     /home/akroshko/cic-vcs/bash-stdlib/launch-emacsclient noframe --eval "(other-frame 1)"
+    # true
 else
     wmctrl -x -a emacs.Emacs
 fi
