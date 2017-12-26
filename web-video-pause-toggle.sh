1e9f5a7f3a1d46f6c756bbb74411a534dd7bcafd
Andrew Kroshko <akroshko.public+devel@gmail.com>
2017-12-26 03:34:12 -0600

    Some updates to include twitch.

---
+++ web-video-pause-toggle.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# global function to pause a running Youtube in Conkeror
+# get current window
+CURRENTWINDOW=$(xdotool getwindowfocus)
+CURRENTYOUTUBE=
+# turn off youtube
+xdotool search --name "youtube|twitch" | while IFS= read -r line; do
+    if xdotool search --class conkeror | grep "${line}" >/dev/null; then
+        xdotool windowfocus --sync "${line}";
+        # TODO: very arbitrary delay, perhaps wait for something to return
+        sleep 0.05
+        ${HOME}/bin/conkeror-batch -f web-video-pause-toggle
+        if [[ "${line}" == "${CURRENTWINDOW}" ]]; then
+            CURRENTYOUTUBE=1
+        fi
+    fi
+done
+xdotool windowactivate --sync ${CURRENTWINDOW}
+# TODO: should have an if before xdotool, but couldn't get it working in 5 minutes
+# restore focus if not youtube window
+# if [[ -n "$CURRENTYOUTUBE" ]]; then
+# fi
