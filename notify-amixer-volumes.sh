3f4e54b2ee78f1b2c5e6b3388ac6e2acc8f0bde2
Andrew Kroshko <akroshko.public+devel@gmail.com>
2019-03-12 02:55:44 -0600

    Better error reporting.

--- notify-amixer-volumes.sh
+++ notify-amixer-volumes.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 amixer-volumes () {
+    # TODO: this is necessary to avoid inaccurate display of the volume levels
+    sleep 0.05
     echo $(amixer get Master | grep Front.*Playback | tr "\\n" " " | sed -e 's/Front//g' -e 's/Playback//g' -e 's/\s[0-9]\+//g' -e 's/  */ /g')
 }
-
 notify-send "$(amixer-volumes)" -t 200
