36adc22d9a0e0868dd5779653c6012dd0d4a50c8
Andrew Kroshko <akroshko.public+devel@gmail.com>
2019-09-04 01:11:57 -0600

    Updated read command, no pubkey auth for SSH fallback, improved syntax, and synced environment variables with crypt-profiles..

--- launch-emacsclient-fail.sh
+++ launch-emacsclient-fail.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 # this is an alternate emacsclient, except it simply notifies of the failure,
 # having a seperate script avoids opening Emacs without the server
 notify-send 'emacsclient failed to start!!!' -t 5000
--- notify-amixer-volumes.sh
+++ notify-amixer-volumes.sh
@@ -3,6 +3,6 @@
 amixer-volumes () {
     # delay required after changing volume to avoid inaccurate display of mixer levels
     sleep 0.15
-    echo $(amixer get Master | grep --color=never  'Front.*Playback' | tr "\\n" " " | sed -e 's/Front//g' -e 's/Playback//g' -e 's/\s[0-9]\+//g' -e 's/  */ /g')
+    amixer get Master | grep --color=never  'Front.*Playback' | tr "\\n" " " | sed -e 's/Front//g' -e 's/Playback//g' -e 's/\s[0-9]\+//g' -e 's/  */ /g'
 }
 notify-send "$(amixer-volumes)" -t 200
